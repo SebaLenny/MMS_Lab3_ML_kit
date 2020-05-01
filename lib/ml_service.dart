986313b93b39d6ab97119ac51204914418c2c43b
sebastian-idziak <i.basti@wp.com>
2020-05-01 11:47:26 +0200

    Implemented labels detector

--- lib/ml_service.dart
+++ lib/ml_service.dart
@@ -6,7 +6,14 @@ class MlService {
   Future<VisionText> getTextBlocksFromImage(File imageFile) async {
     final image = FirebaseVisionImage.fromFile(imageFile);
     final textDecetor = FirebaseVision.instance.textRecognizer();
-    var visionText = await textDecetor.processImage(image);
+    final visionText = await textDecetor.processImage(image);
     return visionText;
+  }
+
+  Future<List<ImageLabel>> getTextLabelsFromImage(File imageFile) async {
+    final image = FirebaseVisionImage.fromFile(imageFile);
+    final imageLabeler = FirebaseVision.instance.imageLabeler();
+    final labels = await imageLabeler.processImage(image);
+    return labels;
   }
 }
